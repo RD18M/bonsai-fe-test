c052ea9970d239ad6bf409480539f57b632729fa
Rafael Mariscal <dominicmariscal78@gmail.com>
2022-02-18 11:29:24 -0500

    prodct card css changes

--- frontend/src/components/product-card/product-card.tsx
+++ frontend/src/components/product-card/product-card.tsx
@@ -16,15 +16,16 @@ interface IProductCardProps {
 }
 
 const ProductCard: FC<IProductCardProps> = ({ product }): ReactElement => {
-  console.log({ product });
+  const { defaultImage, description, name } = product;
 
   return (
     <div className="product-card-container">
-      {/* <img src={imageSrc} /> */}
+      <img src={defaultImage} alt="Product Logo" />
       <div className="product-card-details">
-        {/* <span className="product-name">{name} </span>
-        <span className="product-description"> {description} </span> */}
+        <span className="product-name">{name} </span>
+        <span className="product-description"> {description} </span>
       </div>
+      <button>Add to Cart</button>
     </div>
   );
 };
